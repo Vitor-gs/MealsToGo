f1f790e9deab1755e0b8ff3ead19c40847064fdb
VitorSantos <vitor@energiaautomacao.com.br>
2022-08-09 13:43:16 -0300

    new_part

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,31 +1,46 @@
-import React from "react";
-import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, View } from "react-native";
+import React, { useContext } from "react";
+import { ActivityIndicator, Colors } from "react-native-paper";
+import { FlatList, Pressable } from "react-native";
 import styled from "styled-components/native";
+import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Search } from "../components/search.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
 `;
 
-const SearchContainer = styled.View`
-  padding: 16px;
-`;
-
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: 16px;
-  background-color: blue;
-`;
-
-export const RestaurantsScreen = () => (
-  <SafeArea>
-    <SearchContainer>
-      <Searchbar />
-    </SearchContainer>
-    <RestaurantListContainer>
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
-  </SafeArea>
-);
+export const RestaurantsScreen = ({ navigation }) => {
+  const { isLoading, restaurants } = useContext(RestaurantsContext);
+  return (
+    <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
+      <Search />
+      <FlatList
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <Pressable onPress={() => navigation.navigate("RestaurantDetail")}>
+              <Spacer position="bottom" size="large">
+                <RestaurantInfoCard restaurant={item} />
+              </Spacer>
+            </Pressable>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
+    </SafeArea>
+  );
+};
